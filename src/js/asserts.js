c324133bb1cfc562f43279601742b90cde49afd9
moises.alcocer <contacto@ironwoods.es>
2020-02-03 12:29:27 +0100

    added asserts

---
+++ src/js/asserts.js
@@ -0,0 +1,77 @@
+/**
+ * MIT License - Copyright (c) 2020 Moisés Alcocer
+ *
+ * contacto@ironwoods.es
+ * https://www.ironwoods.es
+ */
+
+function assertArray(data)
+{
+    return (checkArray(data));
+}
+
+function assertArraySize(data, expectedSize)
+{
+    return (Array.isArray(data) && data.length === expectedSize);
+}
+
+/**
+ * Checks an empty String, Array or Object
+ *
+ * @param mixed     data
+ */
+function assertEmpty(data)
+{
+    if (data !== undefined && data !== null
+        && typeof (data) === 'object'
+        || typeof (data) === 'string') {
+        if (data === ''
+            || Array.isArray(data) && data.length === 0
+            || Object.entries(data).length === 0) {
+            
+            return true;
+        }
+    }
+
+    return false;
+}
+
+function assertEquals(data, expected, strict=false)
+{
+    return (strict)
+        ? (data === expected)
+        : assertSoftEquals(data, expected);
+}
+
+function assertFalse(data)
+{
+    return (data === false);
+}
+
+/**
+ * Compare two data with NO strict checks for "arrays" and "objects":
+ * only checks the type and size
+ *
+ * @param mixed     data
+ * @param mixed     expected
+ */
+function assertSoftEquals(data, expected)
+{
+    // Use NO strict comparison for "arrays" -> type & size
+    if (checkArray(data) && checkArray(expected)) {
+        return (data.length === expected.length);
+    }
+
+    // Use NO strict comparison for "objects" -> type & size
+    if (checkObject(data) && checkObject(expected)) {
+        return (Object.entries(data).length 
+            === Object.entries(expected).length);
+    }
+
+    return (data === expected);
+}
+
+function assertTrue(data)
+{
+    return (data === true);
+}
